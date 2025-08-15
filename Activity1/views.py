e2da582dffac4b008817011f3254787d2d369552
godwyn20 <gtatlonghari@student.hau.edu.ph>
2025-08-15 20:41:58 +0800

    wip ulet

--- Activity1/views.py
+++ Activity1/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 #     return render(request, "forms.html")
 
 def tweet_detail_view(request, id=1):
-    return render(request, 'tweets/detail_view.html'. {})
+    return render(request, 'tweets/detail_view.html')
 
 def tweet_list_view(request):
-    return render(request, 'tweets/list_view.html'. {})
+    return render(request, 'tweets/list_view.html')
 
